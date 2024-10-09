b0e77e59bd4888ab95416ae4dba2713a4a66a447
zzy <1184271384@qq.com>
2024-10-09 20:17:52 +0800

    fix(20241009):修改错误响应返回json

--- Carbon_backend/schemas/response.py
+++ Carbon_backend/schemas/response.py
@@ -1,7 +1,8 @@
-from typing import Generic, TypeVar, Optional
+from typing import Generic, TypeVar, Optional, Union
 
 from fastapi import HTTPException
 from pydantic import BaseModel, Field
+from starlette.responses import JSONResponse
 
 T = TypeVar("T")
 
@@ -10,14 +11,23 @@ T = TypeVar("T")
 class ResponseBase(BaseModel, Generic[T]):
     code: int = Field(..., description="响应状态码")
     msg: str = Field(..., description="响应信息")
-    data: dict = Field(..., description="响应数据")
+    data: Optional[Union[T, dict]] = Field(None, description="响应数据")
+
 
 # 通用成功响应
-def success_response(data: Optional[dict] = None, message: str = "Success") -> ResponseBase:
+def success_response(data: Optional[Union[T, dict]] = None, message: str = "Success") -> ResponseBase[T]:
     if data is None:
         data = {}
     return ResponseBase(code=200, msg=message, data=data)
 
+
 # 通用错误响应
-def error_response(message: str, code: int = 400) -> HTTPException:
-    return HTTPException(status_code=code, detail=message)
+def error_response(message: str, code: int = 400) -> JSONResponse:
+    return JSONResponse(
+        status_code=code,
+        content={
+            "code": code,
+            "msg": message,
+            "data": None
+        }
+    )
